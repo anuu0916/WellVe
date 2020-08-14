bcace66ea70b5d6b032977813c56d7d8241fedfc
gkskzh100 <a22569447@gmail.com>
2020-08-14 15:30:29 +0900

    TabLayout 추가

---
+++ app/src/main/java/com/diary/jimin/wellve/Page1Fragment.java
@@ -0,0 +1,35 @@
+package com.diary.jimin.wellve;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Page1Fragment extends Fragment {
+
+    public static Page1Fragment getInstance() {
+        Page1Fragment page1Fragment = new Page1Fragment();
+        return page1Fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page1, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/diary/jimin/wellve/Page5Fragment.java
@@ -0,0 +1,34 @@
+package com.diary.jimin.wellve;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Page5Fragment extends Fragment {
+    public static Page5Fragment getInstance() {
+        Page5Fragment page5Fragment = new Page5Fragment();
+        return page5Fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page5, container, false);
+        return view;
+    }
+}
