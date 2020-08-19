276f5d3d06d06f666b0663fd405c9af0cd8b7f48
fjiyt <jy111099@gmail.com>
2020-08-19 14:59:46 +0900

    sync

--- app/src/main/java/com/diary/jimin/wellve/MainActivity.java
+++ app/src/main/java/com/diary/jimin/wellve/MainActivity.java
@@ -3,6 +3,7 @@ package com.diary.jimin.wellve;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,7 +23,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Member;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
