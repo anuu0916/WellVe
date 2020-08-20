1ce5b3d506a1b86eff83f48dd943835551277a33
hongdroid94 <hongdroid94@innodep.com>
2020-08-20 16:47:54 +0900

    1. 전반적인 패키지 정리 2. MultiDex Issue로 인해 Custom Application 생성

---
+++ app/src/main/java/com/diary/jimin/wellve/activity/CommunityActivity.java
@@ -0,0 +1,95 @@
+package com.diary.jimin.wellve.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.diary.jimin.wellve.fragment.Page1Fragment;
+import com.diary.jimin.wellve.fragment.Page2Fragment;
+import com.diary.jimin.wellve.fragment.Page3Fragment;
+import com.diary.jimin.wellve.fragment.Page4Fragment;
+import com.diary.jimin.wellve.fragment.Page5Fragment;
+import com.diary.jimin.wellve.R;
+import com.diary.jimin.wellve.adapter.VPAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+public class CommunityActivity extends AppCompatActivity {
+
+//    private ListView listView;
+//    private CategoryAdapter adapter;
+//    private FirebaseFirestore db;
+//
+//    private List<String> categoryList = new ArrayList<>();
+
+
+    private ViewPager vp;
+    private VPAdapter vpAdapter;
+    private TabLayout tab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+
+
+        vp = findViewById(R.id.categoryViewPager);
+
+
+        getTabs();
+//        listView = (ListView) findViewById(R.id.categoryListView);
+
+//        db = FirebaseFirestore.getInstance();
+//        adapter = new CategoryAdapter();
+//
+//
+//        categoryList.add("freePosts");
+//        categoryList.add("QnAPosts");
+//        categoryList.add("restaurantPosts");
+//        categoryList.add("literPosts");
+//
+//
+//        adapter.addItem("자유게시판");
+//        adapter.addItem("QnA");
+//        adapter.addItem("비건 식당 추천");
+//        adapter.addItem("비건 문학 추천");
+//
+//        listView.setAdapter(adapter);
+//
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(CommunityActivity.this, BoardActivity.class);
+//                intent.putExtra("category", categoryList.get(position));
+//                startActivity(intent);
+//
+//            }
+//        });
+    }
+
+    public void getTabs() {
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                vpAdapter = new VPAdapter(getSupportFragmentManager());
+
+                vpAdapter.addFragment(Page1Fragment.getInstance(),"전체");
+                vpAdapter.addFragment(Page2Fragment.getInstance(),"자유");
+                vpAdapter.addFragment(Page3Fragment.getInstance(),"QnA");
+                vpAdapter.addFragment(Page4Fragment.getInstance(),"식당");
+                vpAdapter.addFragment(Page5Fragment.getInstance(),"문학");
+
+                vp.setAdapter(vpAdapter);
+
+                tab = findViewById(R.id.tab);
+                tab.setupWithViewPager(vp);
+            }
+        });
+
+
+    }
+}
+
---
+++ app/src/main/java/com/diary/jimin/wellve/model/PostInfo.java
@@ -0,0 +1,95 @@
+package com.diary.jimin.wellve.model;
+
+public class PostInfo {
+
+    private String title;
+    private String text;
+    private String id;
+    private String time;
+    private String name;
+    private String postId;
+    private String category;
+
+    public PostInfo(String title, String text, String id, String time, String name) {
+        this.title = title; //게시글 제목
+        this.text = text;   //게시글 내용
+        this.id = id;       //사용자 uid
+        this.time = time;   //쓴 시간
+        this.name = name;   //사용자 닉네임
+    }
+
+    public PostInfo(String text, String id, String time, String name, String category, String postId) {
+        this.text = text;   //코멘트
+        this.id = id;       //사용자 uid
+        this.time = time;   //쓴 시간
+        this.name = name;   //사용자 닉네임
+        this.category = category; //카테고리
+        this.postId = postId; //게시글 uid
+    }
+
+    public PostInfo(String text, String id, String time, String name) {
+        this.text = text;   //코멘트
+        this.id = id;       //사용자 uid
+        this.time = time;   //쓴 시간
+        this.name = name;   //사용자 닉네임
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostId() {
+        return this.postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+
+}
