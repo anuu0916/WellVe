62ae963173e495be6da0e38107d75ea0c578b043
gkskzh100 <a22569447@gmail.com>
2020-08-20 15:03:30 +0900

    BookMarkActivity 수정

--- app/src/main/java/com/diary/jimin/wellve/BookmarkActivity.java
+++ app/src/main/java/com/diary/jimin/wellve/BookmarkActivity.java
@@ -36,6 +36,13 @@ import java.util.List;
 public class BookmarkActivity extends AppCompatActivity {
 
 
+    private ListView bookMarkListView;
+    private ListViewAdapter adapter;
+    private FirebaseFirestore db;
+    private FirebaseUser user;
+    private List<String> titleList = new ArrayList<>();
+    private List<String> categoryList = new ArrayList<>();
+
     private ImageView myPageProfileImage;
     private TextView myPageNickName;
 
@@ -45,6 +52,61 @@ public class BookmarkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bookmark);
 
         init();
+
+        db = FirebaseFirestore.getInstance();
+        adapter = new ListViewAdapter();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        CollectionReference document = db.collection("users").document(user.getUid())
+                .collection("bookmarks");
+
+        DocumentReference document2 = db.collection("users").document(user.getUid());
+
+        document2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()) {
+                        myPageNickName.setText(documentSnapshot.getData().get("nickname")+"님!");
+                        if(documentSnapshot.getData().get("profileImageUrl")==null) {
+                            myPageProfileImage.setImageResource(R.drawable.app_icon);
+                        }
+                    }
+                } else {
+                    Log.d("BookmarkActivity", ""+task.getException());
+                }
+            }
+        });
+
+
+//        document.orderBy("time", Query.Direction.DESCENDING)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if(task.isSuccessful()) {
+//                            for(QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+//                                adapter.addItem(documentSnapshot.get("title").toString(),
+//                                        documentSnapshot.get("text").toString(),
+//                                        documentSnapshot.get("id").toString(),
+//                                        documentSnapshot.get("time").toString(),
+//                                        documentSnapshot.get("name").toString());
+//                                titleList.add(documentSnapshot.getId());
+//                                categoryList.add(documentSnapshot.get("category").toString());
+//
+//                            }
+//
+//                        }
+//                    }
+//                });
+
+
+
+
+
+
+
 
     }
 
