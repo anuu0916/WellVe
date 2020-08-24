0026cba5f16ae58ec5bf4175e232b1326da931c4
yegyeom <69509725+yegyeom@users.noreply.github.com>
2020-08-24 17:09:54 +0900

    비밀번호 찾기

---
+++ app/src/main/java/com/diary/jimin/wellve/activity/FindPasswordActivity.java
@@ -0,0 +1,83 @@
+package com.diary.jimin.wellve.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.diary.jimin.wellve.R;
+
+public class FindPasswordActivity extends AppCompatActivity {
+
+    private Button tmpPwButton;
+    private Button findIDButton;
+    private Button backButton;
+    private EditText findPwEmailEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password);
+
+        init();
+        clickListener();
+
+        tmpPwButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindPasswordActivity.this, FindPasswordCompleteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void init() {
+        tmpPwButton = (Button) findViewById(R.id.tmpPwButton);
+        findIDButton = (Button) findViewById(R.id.findIDButton);
+        backButton = (Button) findViewById(R.id.findPwBackButton);
+        findPwEmailEditText = (EditText) findViewById(R.id.findPwEmailEditText);
+
+        findPwEmailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tmpPwButton.setSelected(true);
+                tmpPwButton.setTextColor(getResources().getColor(R.color.colorWhite));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tmpPwButton.setSelected(true);
+                tmpPwButton.setTextColor(getResources().getColor(R.color.colorWhite));
+            }
+        });
+    }
+
+    private void clickListener() {
+        tmpPwButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (tmpPwButton.isSelected()) {
+                }
+
+            }
+        });
+    }
+
+}
