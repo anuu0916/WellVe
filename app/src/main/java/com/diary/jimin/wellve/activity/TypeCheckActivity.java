04712dd520b5380592cc81be0cb2f3d7110e411a
yegyeom <69509725+yegyeom@users.noreply.github.com>
2020-08-26 17:25:21 +0900

    커밋

--- app/src/main/java/com/diary/jimin/wellve/activity/TypeCheckActivity.java
+++ app/src/main/java/com/diary/jimin/wellve/activity/TypeCheckActivity.java
@@ -1,5 +1,6 @@
 package com.diary.jimin.wellve.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TypeCheckActivity extends AppCompatActivity {
+public class TypeCheckActivity extends Activity {
 
     private Button typePescoButton;
     private Button typeLactoovoButton;
