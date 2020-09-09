e6e8fb08049dc6073f3e5e70fae6e568d588b335
gkskzh100 <a22569447@gmail.com>
2020-09-09 23:29:37 +0900

    ReTypeCheckActivity Fix

--- app/src/main/java/com/diary/jimin/wellve/activity/InfoModifyActivity.java
+++ app/src/main/java/com/diary/jimin/wellve/activity/InfoModifyActivity.java
@@ -1,13 +1,23 @@
 package com.diary.jimin.wellve.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.diary.jimin.wellve.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class InfoModifyActivity extends AppCompatActivity {
 
@@ -16,17 +26,49 @@ public class InfoModifyActivity extends AppCompatActivity {
     private Button nicknameModifyButton;
     private Button backButton;
 
+    private final int GET_GALLERY_IMAGE = 200;
+
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private String nickname;
+    private String photoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_modify);
 
         init();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection("users").document(user.getUid());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()) {
+
+                        if(document.getData().get("nickname") == null) {
+                            nickname = "null";
+                        } else {
+                            nickname = document.getData().get("nickname").toString();
+                        }
+
+                        if(document.getData().get("profileImageUrl") == null) {
+                            photoUri = "null";
+                        } else {
+                            photoUri = document.getData().get("profileImageUrl").toString();
+                        }
+                    }
+                }
+            }
+        });
 
         veganModifyButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(InfoModifyActivity.this, ReTypeCheckActivity.class);
+                intent.putExtra("getNickname", nickname);
+                intent.putExtra("getUri", photoUri);
                 startActivity(intent);
             }
         });
@@ -34,7 +76,10 @@ public class InfoModifyActivity extends AppCompatActivity {
         nicknameModifyButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+
                 Intent intent = new Intent(InfoModifyActivity.this, NicknameModifyActivity.class);
+                intent.putExtra("getNickname", nickname);
+                intent.putExtra("getUri", photoUri);
                 startActivity(intent);
             }
         });
@@ -49,10 +94,25 @@ public class InfoModifyActivity extends AppCompatActivity {
         modifyPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            Log.d("gallery", ""+selectedImageUri);
+
+        }
+
+    }
+
 
     void init(){
         veganModifyButton = (Button)findViewById(R.id.infoVeganModifyButton);
