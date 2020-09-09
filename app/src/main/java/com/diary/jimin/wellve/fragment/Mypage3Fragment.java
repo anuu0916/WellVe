cbf82ab06a0b90545cec9224d9e0bd05996b1911
gkskzh100 <a22569447@gmail.com>
2020-09-09 17:45:06 +0900

    myPageActivity get photo

--- app/src/main/java/com/diary/jimin/wellve/fragment/Mypage3Fragment.java
+++ app/src/main/java/com/diary/jimin/wellve/fragment/Mypage3Fragment.java
@@ -2,7 +2,9 @@ package com.diary.jimin.wellve.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,7 @@ import com.diary.jimin.wellve.model.CommunityItem;
 import com.diary.jimin.wellve.R;
 import com.diary.jimin.wellve.adapter.RecyclerViewAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,6 +30,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,6 +53,8 @@ public class Mypage3Fragment extends Fragment {
 
     private int commentSize;
     private String getCategory;
+
+    private String imageURL;
 
 
     public static Mypage3Fragment getInstance() {
@@ -102,6 +109,7 @@ public class Mypage3Fragment extends Fragment {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if(task.isSuccessful()) {
                             for(QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                Boolean photoBool = (Boolean) documentSnapshot.getData().get("photo");
                                 db.collection("comments")
                                         .whereEqualTo("postId", documentSnapshot.getId())
                                         .get()
@@ -123,17 +131,46 @@ public class Mypage3Fragment extends Fragment {
                                                         }
                                                         commentSize++;
                                                     }
-                                                    items.add(new CommunityItem(documentSnapshot.getData().get("name").toString(),
-                                                            "https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/039/143/medium/IMG_5649%E3%81%AE%E3%82%B3%E3%83%92%E3%82%9A%E3%83%BC.jpg?2019",
-                                                            documentSnapshot.getData().get("title").toString(),
-                                                            documentSnapshot.getData().get("time").toString(),
-                                                            getCategory,
-                                                            String.valueOf(commentSize)
-                                                    ));
-                                                    idList.add(documentSnapshot.getId());
-                                                    categoryList.add(documentSnapshot.get("category").toString());
-                                                    recyclerView.setAdapter(adapter);
-                                                    progressBar.setVisibility(View.GONE);
+
+                                                    Log.d("mypage", photoBool+"");
+                                                    if (photoBool) {
+                                                        String imageStr = documentSnapshot.getData().get("time").toString().replace("/","").replace(" ","_").replace(":","");
+                                                        String newUri = imageStr.substring(0, imageStr.length()-2);
+                                                        FirebaseStorage storage = FirebaseStorage.getInstance("gs://wellve.appspot.com");
+                                                        StorageReference storageReference = storage.getReference().child(documentSnapshot.getData().get("title")+"_"+newUri+".jpeg");
+                                                        Log.d("mypage",""+storage.getReference().child(documentSnapshot.getData().get("title")+"_"+newUri+".jpeg"));
+                                                        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                            @Override
+                                                            public void onSuccess(Uri uri) {
+                                                                imageURL = uri.toString();
+
+                                                                items.add(new CommunityItem(documentSnapshot.getData().get("name").toString(),
+                                                                        imageURL,
+                                                                        documentSnapshot.getData().get("title").toString(),
+                                                                        documentSnapshot.getData().get("time").toString(),
+                                                                        getCategory,
+                                                                        String.valueOf(commentSize)
+                                                                ));
+//                                                                Log.d("comment", "이미지 O : "+freeSize);
+                                                                idList.add(documentSnapshot.getId());
+                                                                categoryList.add("restaurantPosts");
+                                                                recyclerView.setAdapter(adapter);
+                                                                progressBar.setVisibility(View.GONE);
+                                                            }
+                                                        });
+                                                    } else {
+                                                        items.add(new CommunityItem(documentSnapshot.getData().get("name").toString(),
+                                                                null,
+                                                                documentSnapshot.getData().get("title").toString(),
+                                                                documentSnapshot.getData().get("time").toString(),
+                                                                getCategory,
+                                                                String.valueOf(commentSize)
+                                                        ));
+                                                        idList.add(documentSnapshot.getId());
+                                                        categoryList.add("restaurantPosts");
+                                                        recyclerView.setAdapter(adapter);
+                                                        progressBar.setVisibility(View.GONE);
+                                                    }
 
                                                 }
                                             }
